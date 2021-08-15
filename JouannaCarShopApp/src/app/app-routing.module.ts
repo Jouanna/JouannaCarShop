68d14d61dbb123152a94cd8bf8a77a5bcda68ab9
Jouanna <74823299+Jouanna@users.noreply.github.com>
2021-08-15 16:56:15 +0300

    Components created and routes

--- JouannaCarShopApp/src/app/app-routing.module.ts
+++ JouannaCarShopApp/src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllComponent } from './cars/all/all.component';
+import { CreateComponent } from './cars/create/create.component';
+import { DetailComponent } from './cars/detail/detail.component';
+import { HomeComponent } from './core/home/home.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { LoginComponent } from './users/login/login.component';
+import { RegisterComponent } from './users/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'cars',
+    component: AllComponent
+  },
+  {
+    path: 'cars/:objectId',
+    component: DetailComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- JouannaCarShopApp/src/app/app.module.ts
+++ JouannaCarShopApp/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './users/login/login.component';
+import { RegisterComponent } from './users/register/register.component';
+import { AllComponent } from './cars/all/all.component';
+import { DetailComponent } from './cars/detail/detail.component';
+import { CreateComponent } from './cars/create/create.component';
+import { HomeComponent } from './core/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AllComponent,
+    DetailComponent,
+    CreateComponent,
+    HomeComponent,
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
